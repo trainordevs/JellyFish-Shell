6e12fa6b8fa06130463f178d9670f94abf12ac35
Robert Trainor <44984907+trainordevelopments@users.noreply.github.com>
2021-11-28 20:13:35 -0600

    Ban Manager
    The ban manager has been moved from jf-core to jf-banmanager.

---
+++ resources/[jf]/jf-banmanager/server/sv_banmanager.lua
@@ -0,0 +1,42 @@
+AddEventHandler('playerConnecting', function()
+    local player = source
+    local identifiers = GetPlayerIdentifiers(player)
+    local rsLicense
+
+    for k,v in pairs(identifiers) do
+        if string.sub(v, 1, string.len("license:")) == "license:" then
+            rsLicense = v
+        end
+    end
+
+    if not rsLicense then
+        DropPlayer("We were unable to fetch your Rockstar License.")
+        CancelEvent()
+    end
+
+    exports.oxmysql:execute('SELECT * FROM bans WHERE license = ?', { rsLicense }, function(result)
+        if result[1] then
+            local ban_expire
+            local ban_id
+            local ban_reason
+
+            for _, v in pairs(result) do
+                ban_expire = v.ban_expire
+                ban_id = v.ban_id
+                ban_reason = v.ban_reason
+            end
+            
+            if ban_expire >= os.time() or ban_expire == -1 then
+                if ban_expire == -1 then
+                    print(':jf-banmanager: (sv_banmanager.lua) - License (' .. rsLicense .. ') is PERMA banned; Dropping the player.')
+                    reason = ':JFC: You are banned from this server. \n\nBanID: ' .. ban_id .. '\nReason: ' .. ban_reason .. '\nExpires: Permanent.\n\n'
+                else
+                    print(':jf-banmanager: (sv_banmanager.lua) - License (' .. rsLicense .. ') is banned until '.. ban_expire .. '; Dropping the player.')
+                    reason = ':JFC: You are banned from this server. \n\nBanID: ' .. ban_id .. '\nReason: ' .. ban_reason .. '\nExpires: ' .. ban_expire .. '.\n\n'
+                end
+                DropPlayer(player, reason)
+                CancelEvent()
+            end
+        end
+    end)
+end)
--- resources/[jf]/jf-core/fxmanifest.lua
+++ resources/[jf]/jf-core/fxmanifest.lua
@@ -1,9 +1,7 @@
 fx_version 'cerulean'
 game 'gta5'
 
-author 'An awesome dude'
-description 'An awesome, but short, description'
-version '1.0.0'
+author 'TrainorDevelopments'
 client_scripts {
     'utils/cl_utils.lua',
     'misc/cl_jumping.lua'
@@ -13,18 +11,9 @@ server_scripts {
     'utils/sv_utils.lua',
     'misc/sv_resources.lua',
     'misc/sv_versioncheck.lua',
-    'banmanager/sv_banmanager.lua',
     'playermanager/sv_playermanager.lua'
 }
 
 dependencies {
     'oxmysql'
-}
-
-exports {
-
-}
-
-server_exports {
-
 }
