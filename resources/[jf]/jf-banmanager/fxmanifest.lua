7a67e8c353f8b8c5f3713b59a2f327a8dffcc462
Robert Trainor <44984907+trainordevelopments@users.noreply.github.com>
2021-11-28 20:42:07 -0600

    Ban Manager
    [1] Added exports.

--- resources/[jf]/jf-banmanager/fxmanifest.lua
+++ resources/[jf]/jf-banmanager/fxmanifest.lua
@@ -8,3 +8,8 @@ server_script 'server/sv_banmanager.lua'
 dependencies {
     'oxmysql'
 }
+
+server_export 'jf_isPlayerBanned'
+server_export 'jf_getBanReason'
+server_export 'jf_getBanExpire'
+server_export 'jf_getBanId'
