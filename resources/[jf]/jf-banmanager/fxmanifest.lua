6961988ba81f465f501bdfe03ab209c70bf413e4
Robert Trainor <44984907+trainordevelopments@users.noreply.github.com>
2021-12-09 17:26:48 -0600

    Update fxmanifest.lua
    [1] Updated author.
    [2] Removed server exports.

--- resources/[jf]/jf-banmanager/fxmanifest.lua
+++ resources/[jf]/jf-banmanager/fxmanifest.lua
@@ -1,15 +1,10 @@
 fx_version 'cerulean'
 game 'gta5'
 
-author 'TrainorDevelopments'
+author 'Trainor Devs'
 
 server_script 'server/sv_banmanager.lua'
 
 dependencies {
     'oxmysql'
 }
-
-server_export 'jf_isPlayerBanned'
-server_export 'jf_getBanReason'
-server_export 'jf_getBanExpire'
-server_export 'jf_getBanId'
