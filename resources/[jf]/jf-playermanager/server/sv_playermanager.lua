a18efd63999b3fd859207341c3e7b4608cedc468
Robert Trainor <44984907+trainordevelopments@users.noreply.github.com>
2021-12-09 17:30:23 -0600

    Update sv_playermanager.lua
    [1] Import JellyFish object.
    [2] Created a function to retrieve the rockstar license of a player as the framework will be dependent on that.
    [3] Moved functions from the banmanager over to playermanager.

--- resources/[jf]/jf-playermanager/server/sv_playermanager.lua
+++ resources/[jf]/jf-playermanager/server/sv_playermanager.lua
@@ -1,4 +1,6 @@
-AddEventHandler('playerConnecting', function(playername)
+local JellyFish = exports['jf-core']:GetObject()
+
+function JellyFish.Players.GetRockstarLicense()
     local player = source
     local identifiers = GetPlayerIdentifiers(player)
     local rsLicense
@@ -8,6 +10,88 @@ AddEventHandler('playerConnecting', function(playername)
             rsLicense = v
         end
     end
+
+    if not rsLicense then
+        return false
+    end
+
+    return rsLicense
+end
+
+function JellyFish.Players.CheckBanStatus(rsLicense)
+    if not rsLicense then
+        DropPlayer("We were unable to fetch your Rockstar License.")
+        CancelEvent()
+    end
+
+    exports.oxmysql:execute('SELECT * FROM bans WHERE license = ?', { rsLicense }, function(result)
+        if result[1] then
+            return true
+        end
+    end)
+
+    return false
+end
+
+function JellyFish.Players.getBanReason(rsLicense)
+    if not rsLicense then
+        DropPlayer("We were unable to fetch your Rockstar License.")
+        CancelEvent()
+    end
+
+    exports.oxmysql:execute('SELECT * FROM bans WHERE license = ?', { rsLicense }, function(result)
+        if result[1] then
+            local ban_reason
+
+            for _, v in pairs(result) do
+                ban_reason = v.ban_reason
+            end
+
+            return ban_reason
+        end
+    end)
+end
+
+function JellyFish.Players.getBanExpire(rsLicense)
+    if not rsLicense then
+        DropPlayer("We were unable to fetch your Rockstar License.")
+        CancelEvent()
+    end
+
+    exports.oxmysql:execute('SELECT * FROM bans WHERE license = ?', { rsLicense }, function(result)
+        if result[1] then
+            local ban_expire
+
+            for _, v in pairs(result) do
+                ban_expire = v.ban_expire
+            end
+
+            return ban_expire
+        end
+    end)
+end
+
+function JellyFish.Players.getBanId(rsLicense)
+    if not rsLicense then
+        DropPlayer("We were unable to fetch your Rockstar License.")
+        CancelEvent()
+    end
+
+    exports.oxmysql:execute('SELECT * FROM bans WHERE license = ?', { rsLicense }, function(result)
+        if result[1] then
+            local ban_id
+
+            for _, v in pairs(result) do
+                ban_id = v.ban_id
+            end
+
+            return ban_id
+        end
+    end)
+end
+
+AddEventHandler('playerConnecting', function(playername)
+    local rsLicense = JellyFish.Players.GetRockstarLicense(src)
 
     if not rsLicense then
         DropPlayer("We were unable to fetch your Rockstar License.")
