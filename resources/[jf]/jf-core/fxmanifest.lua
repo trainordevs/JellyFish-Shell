077b1a27b901c74e33a2d77f9f8b0f2755074704
Robert Trainor <44984907+trainordevelopments@users.noreply.github.com>
2021-12-09 17:27:46 -0600

    Update fxmanifest.lua
    [1] Changed author.
    [2] Added new server script.

--- resources/[jf]/jf-core/fxmanifest.lua
+++ resources/[jf]/jf-core/fxmanifest.lua
@@ -1,13 +1,15 @@
 fx_version 'cerulean'
 game 'gta5'
 
-author 'TrainorDevelopments'
+author 'Trainor Devs'
+
 client_scripts {
     'utils/cl_utils.lua',
     'misc/cl_jumping.lua'
 }
 
 server_scripts {
+    'server/main.lua'
     'utils/sv_utils.lua',
     'misc/sv_resources.lua',
     'misc/sv_versioncheck.lua',
